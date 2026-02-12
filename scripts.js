6793ef1230929766a639c8e762171259e73d7914
SrGabriel-KL <gabrielsenasena234@gmail.com>
2026-02-12 13:09:45 -0300

    atualizado

--- scripts.js
+++ scripts.js
@@ -1,3 +1,49 @@
 const convertButton = document.querySelector(".convert-button")
+const moedaSelect = document.querySelector(".moeda-select")
 
-console.log(convertButton)
+function convertValues() {
+    const inputMoedaValor = document.querySelector(".input-moeda").value
+    const valorMoedaConvertida = document.querySelector(".valor-moeda-convertida")
+    const valorMoeda = document.querySelector(".valor-moeda")
+
+    const dolarToday = 5.2
+    const euroToday = 6.2
+
+    const convertedValue = inputMoedaValor / dolarToday
+
+
+    if (moedaSelect.value == "dolar") {
+        valorMoedaConvertida.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputMoedaValor / dolarToday)
+    }
+    if (moedaSelect.value == "euro") {
+        valorMoedaConvertida.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputMoedaValor / euroToday)
+    }
+
+
+    valorMoedaConvertida.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputMoedaValor)
+
+
+    valorMoeda.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+
+
+    valorMoeda.innerHTML = new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR"
+    }).format(convertedValue)
+
+
+}
+
+convertButton.addEventListener("click", convertValues)
